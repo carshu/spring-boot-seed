ecb488cd0a696c9a9493a50046c2b3abc7204361
卡虚 <qinyong.yqy@alibaba-inc.com>
2019-11-27 20:28:24 +0800

    配置文件

---
+++ src/main/java/com/github/carshu/seed/Application.java
@@ -0,0 +1,17 @@
+package com.github.carshu.seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yeqinyong
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
